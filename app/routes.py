5451e0aeb697a86bbfe4e9060f77b1c612e0d1ff
mike mazara <mike.mazara@gmail.com>
2019-02-02 12:25:41 -0800

    config

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+    #cool comments
